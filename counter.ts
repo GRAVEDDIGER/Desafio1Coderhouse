7997c8eaa4873b45e17ca8fa4d68a8da73a3e593
Adrian Daniel Abadin <AABADIN@GMAIL.COM>
2023-06-06 10:36:12 -0300

    Delete counter.ts

--- counter.ts
+++
@@ -1,9 +0,0 @@
-import { Counters } from "../entities/Utility.Classes";
-
-export class Counter extends Counters{
-
-}
-const adrian= new Counter("Adrian")
-const daniel = new Counter("Daniel")
-console.log(adrian.contar())
-console.log(daniel.contar(),daniel.getResponsable())
